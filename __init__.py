bfd352e328bb4d330773d192fa9c8bd84533c0c2
iTecAI <matteovh@gmail.com>
2019-12-19 06:12:56 -0500

    make logs

--- __init__.py
+++ __init__.py
@@ -1,8 +1,9 @@
-from json import load, loads
+from json import *
 from htc import Element, CSS
 from github import Github
 from threading import Thread
 import sys
+import time
 
 def run_instance(settings):
     if settings['token']:
@@ -11,8 +12,13 @@ def run_instance(settings):
         git = Github(settings['username'],settings['password'])
     
     repo = git.get_repo(settings['repo-name'])
-    logs = loads(repo.get_contents(settings['site']['md-dir']+'/log.json').decoded_content)
-    print(logs)
+    try:
+        logs = loads(repo.get_contents(settings['site']['md-dir']+'/log.json').decoded_content)
+    except:
+        repo.create_file(settings['site']['md-dir']+'/log.json','Automated creation of log file',dumps({'logs':[]}))
+        logs = loads(repo.get_contents(settings['site']['md-dir']+'/log.json').decoded_content)
+    
+        
 
 if __name__ == '__main__':
     with open('config.json','r') as f:
