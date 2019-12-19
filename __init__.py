4f65be3b74519d1a0d3798cc6cdeea313bb9726c
iTecAI <matteovh@gmail.com>
2019-12-19 07:07:51 -0500

    wip

--- __init__.py
+++ __init__.py
@@ -4,6 +4,8 @@ from github import Github
 from threading import Thread
 import sys
 import time
+from markdown2 import markdown
+from bs4 import BeautifulSoup, NavigableString
 
 def run_instance(settings):
     if settings['token']:
@@ -15,8 +17,29 @@ def run_instance(settings):
     try:
         logs = loads(repo.get_contents(settings['site']['md-dir']+'/log.json').decoded_content)
     except:
+        print('Log file doesn\'t exist, creating...')
         repo.create_file(settings['site']['md-dir']+'/log.json','Automated creation of log file',dumps({'logs':[]}))
         logs = loads(repo.get_contents(settings['site']['md-dir']+'/log.json').decoded_content)
+
+    while True:
+        files = repo.get_contents(settings['site']['md-dir'])
+        for f in files:
+            if f.name == 'log.json':
+                pass
+            elif f.name in logs['logs']:
+                pass
+            else:
+                data = markdown(f.decoded_content)
+                htmldoc = repo.get_contents(settings['site']['path-to-index']).decoded_content
+                soup = BeautifulSoup(htmldoc,'html.parser')
+                post_div_tag = soup.select(settings['site']['post-div'])
+                post = soup.new_tag('div',attrs={'class':'post-box'})
+                post.append(NavigableString(data))
+                post_div_tag.append(post)
+                print(soup.prettify())
+                logs['logs'].append(f.name)
+
+        time.sleep(settings['check-interval'])
     
         
 
